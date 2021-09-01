04a4997be8403f15b03dcc52a11db3ce82a96cc1
AlejandraCeniceros <53828096+AlejandraCeniceros@users.noreply.github.com>
2021-08-31 22:33:48 -0700

    updates

---
+++ Propinas/Propinas/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Propinas
+//
+//  Created by Alumno on 8/31/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var txtTotalCuenta: UITextField!
+    @IBOutlet weak var sldPorcentajePropina: UISlider!
+    @IBOutlet weak var lblPorcentajePropina: UILabel!
+    @IBOutlet weak var lblCantidadPropina: UILabel!
+    @IBOutlet weak var lblCantidadTotalMasPropina: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func doChangePorcentajePropina(_ sender: Any) {
+        lblPorcentajePropina.text = "\(String(Int(sldPorcentajePropina.value)))%"
+        if txtTotalCuenta.text != nil && txtTotalCuenta.text != "" {
+            
+            let cuenta = Float(txtTotalCuenta.text!)!
+            let porcentaje = Float(Int(sldPorcentajePropina.value))
+            let propina = cuenta * (porcentaje / 100.0)
+            lblCantidadPropina.text = "$\(String(format: "%.2f", propina))"
+            lblCantidadTotalMasPropina.text = "$\(String(format: "%.2f", cuenta + propina ))"
+        }}
+    
+    @IBAction func doEdithEndTotalCuenta(_ sender: Any) {
+        
+        }
+    }
